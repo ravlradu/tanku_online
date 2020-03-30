b57c93dba5ea59aeb1450bdfe54d28e06cd8c476
ravlradu <raul@indigenious.ro>
2020-03-30 11:16:03 +0300

    Layout updates

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -668,17 +668,36 @@
             $(this).parents().siblings().find('#cat_toggle.has-sub > a').removeClass('open');
     });
 
+    /*---shop grid activation---*/
+    $('.shop_toolbar_btn > button').on('click', function (e) {
 
-    /*---MailChimp---*/
-    $('#mc-form').ajaxChimp({
-        language: 'en',
-        callback: mailChimpResponse,
-        // ADD YOUR MAILCHIMP URL BELOW HERE!
-        url: 'http://devitems.us11.list-manage.com/subscribe/post?u=6bbb9b6f5827bd842d9640c82&amp;id=05d85f18ef'
+		e.preventDefault();
 
-    });
+    $('.shop_toolbar_btn > button').removeClass('active');
+		$(this).addClass('active');
 
-    });
+		var parentsDiv = $('.shop_wrapper');
+		var viewMode = $(this).data('role');
+
+
+		parentsDiv.removeClass('grid_3 grid_4 grid_5 grid_list').addClass(viewMode);
+
+		if(viewMode == 'grid_3'){
+			parentsDiv.children().addClass('col-lg-4 col-md-4 col-sm-6').removeClass('col-lg-3 col-cust-5 col-12');
+
+		}
+
+		if(viewMode == 'grid_4'){
+			parentsDiv.children().addClass('col-lg-3 col-md-4 col-sm-6').removeClass('col-lg-4 col-cust-5 col-12');
+		}
+
+        if(viewMode == 'grid_list'){
+			parentsDiv.children().addClass('col-12').removeClass('col-lg-3 col-lg-4 col-md-4 col-sm-6 col-cust-5');
+		}
+
+	});
+
+});
     function mailChimpResponse(resp) {
 
         if (resp.result === 'success') {
@@ -717,34 +736,7 @@
     categorySubMenuToggle();
 
 
-    /*---shop grid activation---*/
-    $('.shop_toolbar_btn > button').on('click', function (e) {
 
-		e.preventDefault();
-
-        $('.shop_toolbar_btn > button').removeClass('active');
-		$(this).addClass('active');
-
-		var parentsDiv = $('.shop_wrapper');
-		var viewMode = $(this).data('role');
-
-
-		parentsDiv.removeClass('grid_3 grid_4 grid_5 grid_list').addClass(viewMode);
-
-		if(viewMode == 'grid_3'){
-			parentsDiv.children().addClass('col-lg-4 col-md-4 col-sm-6').removeClass('col-lg-3 col-cust-5 col-12');
-
-		}
-
-		if(viewMode == 'grid_4'){
-			parentsDiv.children().addClass('col-lg-3 col-md-4 col-sm-6').removeClass('col-lg-4 col-cust-5 col-12');
-		}
-
-        if(viewMode == 'grid_list'){
-			parentsDiv.children().addClass('col-12').removeClass('col-lg-3 col-lg-4 col-md-4 col-sm-6 col-cust-5');
-		}
-
-	});
 
     /*---search box slideToggle---*/
     $(".search_box > a").on("click", function() {
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,10 @@ class HomeController < ApplicationController
   def index
     set_basket_data
     @main_categories = Category.where(parent_id:nil)
-    @products = Product.where('available_count > 0')
+    if params[:categorie]
+      @products = Product.where(category:params[:categorie]).where('available_count > 0')
+    else
+      @products = Product.where('available_count > 0')
+    end
   end
 end
