46fd48e7ef243c967872724a7463f97d4921dda9
Alex Deva <alxx@indigenious.ro>
2020-04-02 09:20:27 +0200

    fixed auth

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -6,8 +6,7 @@ class AuthenticationController < ApplicationController
 
   def login
     u = User.where(email: params[:email]).first.try(:authenticate, params[:password])
-    Rails.logger.info "#{u.inspect}"
-    if u.nil?
+    unless u
       logger.error "Login failed"
       flash[:notice] = 'Autentificare nereușită'
       redirect_to admin_index_path
